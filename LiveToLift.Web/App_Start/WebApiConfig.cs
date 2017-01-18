380e4ae107403b59a35f27ae250b897e4012d04a
bonchovylkov <bonchovylkov@abv.bg>
2017-01-18 14:46:24 +0200

    add cors to enable request from other urls

--- LiveToLift.Web/App_Start/WebApiConfig.cs
+++ LiveToLift.Web/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace LiveToLift.Web
 {
@@ -19,6 +20,9 @@ namespace LiveToLift.Web
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
